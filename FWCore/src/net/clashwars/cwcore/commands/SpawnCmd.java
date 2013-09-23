5a68e88166601957a53235b7f67adb7e1dba2970
FantasyWar <mc_fantasywar@hotmail.com>
2013-09-23 20:28:13 +0200

    Added player arg to /spawn And -s for silent.

--- FWCore/src/net/clashwars/cwcore/commands/SpawnCmd.java
+++ FWCore/src/net/clashwars/cwcore/commands/SpawnCmd.java
@@ -25,22 +25,39 @@ public class SpawnCmd implements CommandClass {
 		/* Modifiers */
 		if (CmdUtils.hasModifier(args,"-h", false)) {
 			sender.sendMessage(ChatColor.DARK_GRAY + "=====  " + ChatColor.DARK_RED + "CW Command help for: " + ChatColor.GOLD + "/"  + lbl + ChatColor.DARK_GRAY + "  =====");
-			sender.sendMessage(pf + "Usage: " + ChatColor.DARK_PURPLE + "/spawn");
-			sender.sendMessage(pf + "Desc: " + ChatColor.GRAY + "Teleport to the spawnpoint");
+			sender.sendMessage(pf + "Usage: " + ChatColor.DARK_PURPLE + "/spawn [player]");
+			sender.sendMessage(pf + "Desc: " + ChatColor.GRAY + "Teleport a player to the spawnpoint");
+			sender.sendMessage(pf + "Modifiers: ");
+			sender.sendMessage(ChatColor.DARK_PURPLE + "-s" + ChatColor.DARK_GRAY + " - " + ChatColor.GRAY + "No messages");
 			return true;
+		}
+		boolean silent = false;
+		if (CmdUtils.hasModifier(args,"-s", true)) {
+			silent = true;
+			args = CmdUtils.modifiedArgs(args,"-s", true);
 		}
 		
 		/* Console check */
 		if (!(sender instanceof Player)) {
-			sender.sendMessage(pf + ChatColor.RED + "Only players can use this command.");
+			if (args.length < 1) {
+				sender.sendMessage(pf + ChatColor.RED + "You need to specify a player to use this on the console!");
+				return true;
+			}
 			return true;
 		} else {
 			player = (Player) sender;
 		}
 		
+		
+		if (args.length > 0) {
+			player = cwc.getServer().getPlayer(args[0]);
+		}
+		
 		/* action */
 		player.teleport(cwc.getServer().getWorld(player.getWorld().getName()).getSpawnLocation());
-		player.sendMessage(pf + "Teleporting to spawn...");
+		if (!silent) {
+			player.sendMessage(pf + "Teleporting to spawn...");
+		}
 		return true;
 	}
 
